3aa12e0e2860990dece6c9017463f0d519c96bb3
Rihab <rihabcheberli@insat.u-carthage.tn>
2022-02-13 19:54:45 +0100

    first element component

---
+++ polyglart-master/Frontend/src/app/home/first-element/first-element.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FirstElementComponent } from './first-element.component';
+
+describe('FirstElementComponent', () => {
+  let component: FirstElementComponent;
+  let fixture: ComponentFixture<FirstElementComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FirstElementComponent ]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FirstElementComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ polyglart-master/Frontend/src/app/home/first-element/first-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-element',
+  templateUrl: './first-element.component.html',
+  styleUrls: ['./first-element.component.css']
+})
+export class FirstElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
