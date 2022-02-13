bb483170b804a62c44879e81f4c8c09476bfe857
Anas Hamrouni <noussahouma@gmail.com>
2022-02-13 18:22:56 +0100

    fixed apiUrl

--- polyglart-master/Frontend/src/app/services/user.service.ts
+++ polyglart-master/Frontend/src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import {environment} from '../../environments/environment';
 import {User} from '../shared/user';
 
 @Injectable({ providedIn: 'root' })
@@ -8,14 +8,14 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   getAll() {
-    return this.http.get<User[]>(`${config.apiUrl}/users`);
+    return this.http.get<User[]>(`${environment.apiUrl}/users`);
   }
 
   register(user: User) {
-    return this.http.post(`${config.apiUrl}/users/register`, user);
+    return this.http.post(`${environment.apiUrl}/users/register`, user);
   }
 
   delete(id: number) {
-    return this.http.delete(`${config.apiUrl}/users/${id}`);
+    return this.http.delete(`${environment.apiUrl}/users/${id}`);
   }
 }
