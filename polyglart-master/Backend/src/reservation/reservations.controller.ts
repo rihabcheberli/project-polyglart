9a8afed84b1b23d255b18607a8ca8858b34053ee
wafaselmi <wafa.selmi@insat.u-carthage.tn>
2022-02-13 20:24:18 +0100

    reservation controller

---
+++ polyglart-master/Backend/src/reservation/reservations.controller.ts
@@ -0,0 +1,11 @@
+// @ts-ignore
+import { Body, Controller, Post} from '@nestjs/common';
+
+import {CreateReservationDto} from "./create-reservation.dto";
+@Controller('reservations')
+export class ReservationsController {
+    @Post()
+    async addReservation(@Body() CreateReservationDto: CreateReservationDto) {
+        return 'Your reservation was added';
+    }
+}
