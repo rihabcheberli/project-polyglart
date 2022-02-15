6f69fe2f381f5f69903c05b8f8968e80f1e915a6
HamrouniAnas <50056752+HamrouniAnas@users.noreply.github.com>
2022-02-15 16:39:26 +0100

    add reservations

--- polyglart-master/Backend/src/reservation/dto/create-reservation.dto.ts
+++ polyglart-master/Backend/src/reservation/dto/create-reservation.dto.ts
@@ -1,5 +1,5 @@
-import { User } from '../user/User.entity';
-import { Site } from '../sites/Site.entity';
+import { User } from '../../user/User.entity';
+import { Site } from '../../sites/Site.entity';
 
 export class CreateReservationDto {
   _id: number;
---
+++ polyglart-master/Backend/src/reservation/reservation.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { CreateReservationDto } from './dto/create-reservation.dto';
+@Controller('reservations')
+export class ReservationController {
+  @Post()
+  async addReservation(
+    @Body() CreateReservationDto: CreateReservationDto,
+    req,
+    res,
+  ) {
+    return 'Your reservation was added';
+  }
+}
